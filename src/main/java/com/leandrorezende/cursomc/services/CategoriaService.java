18923e91abe1f37aef5ced1e4adbe87bdf6a3f09
leandro.rezende <leandro08rezende@gmail.com>
2019-10-02 08:17:58 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/leandrorezende/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrorezende/cursomc/services/CategoriaService.java
@@ -1,10 +1,10 @@
 package com.leandrorezende.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.leandrorezende.cursomc.domain.Categoria;
@@ -41,5 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
