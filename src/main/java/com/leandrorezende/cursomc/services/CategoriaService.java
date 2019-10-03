90fae325b50529bfb1132cfa2409e1667dc3cad8
leandro.rezende <leandro08rezende@gmail.com>
2019-10-03 10:12:25 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/leandrorezende/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrorezende/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leandrorezende.cursomc.domain.Categoria;
+import com.leandrorezende.cursomc.dto.CategoriaDTO;
 import com.leandrorezende.cursomc.repositories.CategoriaRepository;
 import com.leandrorezende.cursomc.services.exceptions.DataIntegrityException;
 import com.leandrorezende.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
